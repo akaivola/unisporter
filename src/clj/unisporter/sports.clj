fe7f76014d390bf83a9f17f020a2ea6f01fe6353
Aki Kaivola <aki.kaivola@gmail.com>
2017-03-21 21:42:03 +0200

    Filter by full activity and optimize fetches as deferred

--- src/clj/unisporter/sports.clj
+++ src/clj/unisporter/sports.clj
@@ -3,10 +3,14 @@
    [unisporter.util.http :as http]
    [clj-time.coerce :as c]
    [clj-time.core :as t]
+   [manifold.deferred :as d]
    [taoensso.timbre :refer [spy debug]]))
 
 (def meilahti? #(= (:venue %) "Meilahden liikuntakeskus"))
 (defn spinning? [item] (= (first (:rooms item)) "Sisäpyöräilysali"))
+
+(defn full? [{:keys [totalReservations maxAttendees]}]
+  (= totalReservations maxAttendees))
 
 (def sport-details-endpoint "https://unisport.fi/yol/web/fi/crud/read/reservable.json"
   ;?id=203182708&details=true
@@ -22,19 +26,23 @@
                           (c/to-local-date)
                           (.toString))
                     (range 1 8))]
-    (-> (pmap
-          (fn [date]
-            (->> (http/get "https://unisport.fi/yol/web/fi/crud/read/event.json"
-                   {:query-params
-                    {:date             date
-                     :past             "false"
-                     :sportHierarchies "825079,6247560"}})
-                :body
-              :items
-              (eduction
-                (comp
-                  (filter meilahti?)
-                  (filter spinning?)
-                  (filter (complement #(not= (:cancelled %))))))))
-          weekrange)
-        flatten)))
+    (->> (apply d/zip
+           (map
+             (fn [date]
+               (d/future
+                 (->> (http/get "https://unisport.fi/yol/web/fi/crud/read/event.json"
+                        {:query-params
+                         {:date             date
+                          :past             "false"
+                          :sportHierarchies "825079,6247560"}})
+                     :body
+                   :items)))
+             weekrange))
+         deref
+         flatten
+         (eduction
+           (comp
+             (filter meilahti?)
+             (filter spinning?)
+             (filter (complement #(not= (:cancelled %))))
+             (filter full?))))))
