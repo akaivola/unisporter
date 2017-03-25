4fc45c01b20b99e775b0fad1980442d688b2430b
Aki Kaivola <aki.kaivola@gmail.com>
2017-03-25 10:16:34 +0200

    Delete auth

---
+++ src/clj/unisporter/reservation.clj
@@ -0,0 +1,24 @@
+(ns unisporter.reservation
+  (:require
+   [unisporter.redis.config :as r]
+   [taoensso.carmine :as car]
+   [taoensso.timbre :refer [spy debug]]))
+
+(defn prefix [& args]
+  (str "/reservations/" (apply str (interpose "/" args))))
+
+(defn reservations [uid]
+  (car/wcar*
+    (car/scan 0 :match (prefix "*" uid))))
+
+(defn delete-reservation [uid activity-id]
+  (car/wcar r/conn (car/del (prefix activity-id uid))))
+
+(defn hour-before-activity-begins [activity]
+  1)
+
+(defn reserve [uid activity]
+  (car/wcar r/conn (car/setex
+                     (prefix (:id activity) uid)
+                     (hour-before-activity-begins activity)
+                     activity)))
